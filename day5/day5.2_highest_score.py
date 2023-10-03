3576ebbb9acda6b281527c98103f1fd6d3ab841f
unknown <mtr.oliveira.me@gmail,com>
2023-10-02 21:28:19 -0300

    Create day5.2_highest_score.py

---
+++ day5/day5.2_highest_score.py
@@ -0,0 +1,11 @@
+# not using any method to pratice the for loops
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
