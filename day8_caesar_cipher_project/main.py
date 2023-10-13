b94e5f157f975dc259cd1b22d367a54c7bce2426
unknown <mtr.oliveira.me@gmail,com>
2023-10-13 12:51:49 -0300

    Create main.py

---
+++ day8_caesar_cipher_project/main.py
@@ -0,0 +1,92 @@
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+from art import logo
+
+restart = True
+print(logo)
+
+
+def caesar(chosen_direction, user_text, shift_amount):
+    final_text = ""
+
+    for char in user_text:
+        if char in alphabet:
+            if char == alphabet[alphabet.index(char)]:
+                if chosen_direction == "encode":
+                    final_text += alphabet[alphabet.index(char) + shift_amount]
+            if chosen_direction == "decode":
+                final_text += alphabet[alphabet.index(char) - shift_amount]
+        else:
+            final_text += char
+
+    print(f"The {chosen_direction}d text is {final_text}")
+
+
+while restart == True:
+    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 26:
+        shift = shift % 26
+
+    caesar(chosen_direction=direction, user_text=text, shift_amount=shift)
+    go_again = input(
+        "\nType 'yes' if you want to go again. Otherwise type 'no':\n"
+    ).lower()
+
+    if go_again == "no":
+        restart = False
