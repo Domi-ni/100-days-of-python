532fb8095a8631d606386738caac24bc7543061f
Matheus Oliveira <112003358+Domi-ni@users.noreply.github.com>
2023-11-23 11:55:39 -0300

    Update main.py

--- day18-turtle_and_GUI/day18_hirst_paint/main.py
+++ day18-turtle_and_GUI/day18_hirst_paint/main.py
@@ -22,34 +22,36 @@ def star_new_line(turtle):
     turtle.pendown()
 
 
-colors = colorgram.extract('color.jpeg', 10)
-rgb_colors = []
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+def change_dot_colors(turtle):
+    colors = colorgram.extract('color.jpeg', 10)
+    rgb_colors = []
+    for color in colors:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        new_color = (r, g, b)
+        rgb_colors.append(new_color)
+    dot_color = random.choice(rgb_colors)
+    return dot_color
 
-dot_color = random.choice(rgb_colors)
-turtle = turtle_module.Turtle()
-turtle.speed(10)
-turtle.hideturtle()
 
-set_to_initial_location(turtle)
+eny_turtle = turtle_module.Turtle()
+eny_turtle.speed(10)
+eny_turtle.hideturtle()
+
+set_to_initial_location(eny_turtle)
 for _ in range(10):
     for __ in range(10):
-        dot_color = random.choice(rgb_colors)
-        turtle.dot(20, dot_color)
-        turtle.penup()
-        turtle.forward(50)
-        turtle.pendown()
-    star_new_line(turtle)
+        eny_turtle.dot(20, change_dot_colors(eny_turtle))
+        eny_turtle.penup()
+        eny_turtle.forward(50)
+        eny_turtle.pendown()
+    star_new_line(eny_turtle)
 
 my_screen = turtle_module.Screen()
 my_screen.exitonclick()
 
-print(turtle.pos())
+print(eny_turtle.pos())
 
 
 
